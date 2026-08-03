ed06e51d26269658adf32ad583dc6db0962d139a
Mike Mansell <mmansell@diamondq.com>
2026-08-03 15:00:06 -0700

    Adjusted the dep_check.bat to not report on dependencyManagement, only dependencies.

--- dep_check.bat
+++ dep_check.bat
@@ -1,2 +1,2 @@
-cmd /C "mvn -DdepStep=1/2 versions:display-dependency-updates -Dmaven.version.rules=file:///c:/DQGitHub/common-maven/master-parent/ruleset.xml"
+cmd /C "mvn -DdepStep=1/2 versions:display-dependency-updates -DprocessDependencyManagement=false -Dmaven.version.rules=file:///c:/DQGitHub/common-maven/master-parent/ruleset.xml"
 cmd /C "mvn -DdepStep=2/2 versions:display-plugin-updates -Dmaven.version.rules=file:///c:/DQGitHub/common-maven/master-parent/ruleset.xml"
